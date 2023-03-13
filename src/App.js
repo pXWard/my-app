409f7f607919d3dc69cdd84b1ea2747b9f03e4aa
Дмитрий Сергеевич Шипулин <chief.dima24@yandex.ru>
2023-03-13 03:40:13 +0300

    ttts

--- src/App.js
+++ src/App.js
@@ -1,17 +1,57 @@
 import React from 'react';
+import './index.css';
+
+class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+      count: 0
+    }
+  }
 
 
-function App() {
+clickHandler = event => {
+let data = event.target.getAttribute('data');// номер квадрата по которому кликнули
+let currentSquares = this.state.squares;
+console.log(currentSquares);
 
-  // function redirect_BALDA(){
-  //   <a href="index.html">efwefp</a>
-  // }
+if(currentSquares[data] == null){
+currentSquares[data] = (this.state.count % 2 == 0) ? 'X' : 'O';
+this.setState({count: this.state.count + 1});
+this.setState({squares: currentSquares});
+}
+else{alert('Клетка уже занята!')}
+}
 
-  return (
-    <div className="App">
-      <a href="wordgame.html">efwefp</a>
-    </div>  
-  );
-} 
+  render() {
+    return (
+      <div className = "tic-tac-toe">
+        <div className = "ttt-grid"onClick={this.clickHandler} data = "0">{this.state.squares[0]}</div>
+        <div className = "ttt-grid"onClick={this.clickHandler} data = "1">{this.state.squares[1]}</div>
+        <div className = "ttt-grid"onClick={this.clickHandler} data = "2">{this.state.squares[2]}</div>
+        <div className = "ttt-grid"onClick={this.clickHandler} data = "3">{this.state.squares[3]}</div>
+        <div className = "ttt-grid"onClick={this.clickHandler} data = "4">{this.state.squares[4]}</div>
+        <div className = "ttt-grid"onClick={this.clickHandler} data = "5">{this.state.squares[5]}</div>
+        <div className = "ttt-grid"onClick={this.clickHandler} data = "6">{this.state.squares[6]}</div>
+        <div className = "ttt-grid"onClick={this.clickHandler} data = "7">{this.state.squares[7]}</div>
+        <div className = "ttt-grid"onClick={this.clickHandler} data = "8">{this.state.squares[8]}</div>
+        
+      </div>
+    );
+  }
+}
+// function App() {
+
+//   // function redirect_BALDA(){
+//   //   <a href="index.html">efwefp</a>
+//   // }
+
+//   return (
+//     <div className="App">
+//       <a href="wordgame.html">efwefp</a>
+//     </div>  
+//   );
+// } 
  
 export default App;
